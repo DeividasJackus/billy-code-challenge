c99583ea98f7648592c325f839d95411d2a108ea
Deividas Jackus <d@jackus.eu>
2019-06-03 15:35:26 +0200

    add base for debugging & logging

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 "use strict";
 
+const debug = require("debug")("challenge");
 const app = require("./server")();
+
+debug(`Starting up in ${process.env.NODE_ENV || "development"} mode`);
 
 // listen for incoming requests
 const server = app.listen(process.env.PORT || 3000, () => {
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 "use strict";
 
 const express = require("express");
+const morgan = require("morgan");
 
 module.exports = () => {
   const app = express();
+
+  // Log incoming requests to stdout
+  app.use(morgan(app.settings.env === "development" ? "dev" : "tiny"));
 
   app.get("/", (req, res) => {
     res.json();
