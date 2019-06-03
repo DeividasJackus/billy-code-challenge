23541b7e151d5fc23136f688717d42902be49c9e
Deividas Jackus <d@jackus.eu>
2019-06-03 17:58:34 +0200

    add logging and error handling

--- server.js
+++ server.js
@@ -2,15 +2,33 @@
 
 const express = require("express");
 const morgan = require("morgan");
+const boom = require("@hapi/boom");
+
+// A wrapper for asynchronous route handlers to ensure errors are caught and normalized
+// This allows us to use async-await handlers in which we simply throw instead of calling next()
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch((err) => next(err.isBoom ? err : boom.badImplementation(err)));
+};
 
 module.exports = () => {
   const app = express();
 
-  // Log incoming requests to stdout
-  app.use(morgan(app.settings.env === "development" ? "dev" : "tiny"));
+  app.set("case sensitive routing", true);
+  app.set("strict routing", true);
 
-  app.get("/", (req, res) => {
-    res.json();
+  // Log incoming requests to stdout
+  app.use(morgan(app.get("env") === "development" ? "dev" : "tiny"));
+
+  // Use a middleware for parsing JSON requests
+  app.use(express.json());
+
+  // Error handling middleware for Express should be loaded after all route handlers are loaded
+  app.use((err, req, res, next) => {
+    if (err.isServer) {
+      // We'll want to log server errors to stdout
+      console.error(err);
+    }
+    return res.status(err.output.statusCode).json(err.output.payload);
   });
 
   return app;
