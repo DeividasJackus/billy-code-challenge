68ce0419455592757622fe96f6b22d915c6a8cef
Deividas Jackus <d@jackus.eu>
2019-06-03 19:35:34 +0200

    fix db initialization

--- db.js
+++ db.js
@@ -5,4 +5,10 @@ const Memory = require("lowdb/adapters/Memory");
 
 const db = low(new Memory());
 
+// Set defaults
+db.defaults({
+  purchases: [],
+  sales: [],
+}).write();
+
 module.exports = db;
