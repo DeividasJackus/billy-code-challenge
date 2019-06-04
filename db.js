837b84983602be3cf32d9b952a1bd2d109ea3deb
Deividas Jackus <d@jackus.eu>
2019-06-04 08:42:58 +0200

    stop preloading the database with sample data

--- db.js
+++ db.js
@@ -8,29 +8,28 @@ const db = low(new Memory());
 // Set defaults
 db.defaults({
   purchases: [
-    {
-      date: "2016-01-01",
-      unitCost: 10,
-      unitCount: 200,
-      unitsSoldCount: 0,
-      unitsSoldValue: 0,
-    },
-    {
-      date: "2016-01-05",
-      unitCost: 15,
-      unitCount: 250,
-      unitsSoldCount: 0,
-      unitsSoldValue: 0,
-    },
-    {
-      date: "2016-01-10",
-      unitCost: 12.5,
-      unitCount: 150,
-      unitsSoldCount: 0,
-      unitsSoldValue: 0,
-    },
+    // {
+    //   date: "2016-01-01",
+    //   unitCost: 10,
+    //   unitCount: 200,
+    //   unitsSoldCount: 0,
+    //   unitsSoldValue: 0,
+    // },
+    // {
+    //   date: "2016-01-05",
+    //   unitCost: 15,
+    //   unitCount: 250,
+    //   unitsSoldCount: 0,
+    //   unitsSoldValue: 0,
+    // },
+    // {
+    //   date: "2016-01-10",
+    //   unitCost: 12.5,
+    //   unitCount: 150,
+    //   unitsSoldCount: 0,
+    //   unitsSoldValue: 0,
+    // },
   ],
-  sales: [], // We're not really using this, but could track sales here
 }).write();
 
 module.exports = db;
